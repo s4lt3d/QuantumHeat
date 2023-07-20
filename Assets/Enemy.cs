6ec31819c5b61899b512f45a13e3aceee626b786
Walter Gordy <walter.gordy@gmail.com>
2023-07-19 18:31:25 -0600

    Adds navmesh, agent, and animations for enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    NavMeshAgent agent;
+    public Transform playerTarget;
+    public float stopDistance = 5f;
+    private Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();    
         SetupRagdoll();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(playerTarget.position);
+        float distance = Vector3.Distance(playerTarget.position, transform.position);
+        if (distance < stopDistance) { 
+            agent.isStopped = true;
+            animator.SetBool("Shoot", true);
+        }
     }
 
     public void SetupRagdoll()
